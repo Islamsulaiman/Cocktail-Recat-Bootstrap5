c1c37481e5f26a4552bb350c963b3fd542e63e8d
islam Sulaiman <ma.eslam@yahoo.com>
2021-10-24 20:26:22 +0300

    initialize all component and pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import React from "react";
+
+import Home from "./pages/home";
+import About from "./pages/about";
+import Error from "./pages/error";
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 function App() {
-  return <button className='btn btn-primary'>Click me</button>;
+  return (
+    <>
+      <Router>
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/about' component={About} />
+
+          <Route path='*' component={Error} />
+        </Switch>
+      </Router>
+    </>
+  );
 }
 
 export default App;
